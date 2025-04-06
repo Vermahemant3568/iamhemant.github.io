4597b4461e4c3df00bccb5408a1bfcb7a0e50a1e
hemant verma <vermahemant3568@gmail.com>
2025-04-06 21:48:22 +0530

    i have add something

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/*--Profile text animation---*/
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed("#typed-text", {
+      strings: ["Frontend Web Developer", "UI Designer", "Creative Coder"],
+      typeSpeed: 60,
+      backSpeed: 40,
+      backDelay: 2000,
+      loop: true
+    });
+  });
+  
